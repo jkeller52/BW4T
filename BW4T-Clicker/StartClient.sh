1eba094e0a7e1e280275677d7001be1c27ffe65d
jkeller.52 <jacobkeller52@gmail.com>
2022-02-01 10:53:13 -0500

    added .sh files

---
+++ BW4T-Clicker/StartClient.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /Users/jacobkeller/BW4T/ && java -jar client3.10.1.jar 
---
+++ BW4T-Clicker/StartServer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /Users/jacobkeller/BW4T/ && java -jar server3.10.1.jar -serverip localhost -serverport 999
